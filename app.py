e2a9ee48dd1bee560e9563bf7e0bf2980d7fed1c
sujal <sujalpathrabe@gmail.com>
2025-02-09 23:02:38 +0530

    nothing there yet

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import langchainChatbot as lcc
+
+
+
+
+def clear_input_field():
+    st.session_state.user_question = st.session_state.user_input
+    st.session_state.user_input = ""
+    
+    
+
+def set_send_input():
+    st.session_state.send_button = True
+    clear_input_field()
+    
+
+
 
 def main():
     st.set_page_config(page_title = "PDF's and more!!" , page_icon = "🤖")
@@ -6,17 +23,28 @@ def main():
     st.title("Multimodal local Chat Application")
     chat_container = st.container()
     
-    user_input = st.text_input("Enter your message" , key = "user_input")
+    
+    if "send_input" not in st.session_state:
+        st.session_state.send_input = False
+    if "user_question" not in st.session_state:    
+        st.session_state.user_question = ""
+    
+    user_input = st.text_input("Enter your message" , key = "user_input" , on_change= set_send_input)
     
     send_button = st.button("Send" , key = "send_button")
+    # if st.button("Send"):
+    #     st.session_state.send_input = True
+    #     clear_input_field()
     
-    if send_button:
-        llm_response = "This is a response from the LLM model"
+    if send_button or st.session_state.send_input:
+        if st.session_state.user_question != "":
         
-        with chat_container:
-            st.chat_message("user").write(user_input)
-            st.chat_message("ai").write("Here is the answer")
-    
+            llm_response = "This is a response from the LLM model"
+            
+            with chat_container:
+                st.chat_message("user").write(st.session_state.user_question)
+                st.chat_message("ai").write("Here is the answer")
+        
 
 if __name__ == "__main__":
     main()
